32c6ef5ed9a14b18b647d3f4ed83c33b4ff92799
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-06-15 13:16:51 -0300

    config first next project

---
+++ registration-system/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Layout from "@/components/Layout";
+
+export default function Home() {
+  return (
+    <div
+      className={`
+        flex justify-center items-center h-screen
+        bg-gradient-to-r from-blue-500 to-purple-500
+        text-white
+    `}
+    >
+      <Layout titulo="Cadastro de Usuários">
+        <span>Conteúdo</span>
+      </Layout>
+    </div>
+  );
+}
